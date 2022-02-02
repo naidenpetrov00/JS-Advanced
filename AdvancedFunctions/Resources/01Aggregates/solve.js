5ef2ec55102506f5c6e507e9dbc12c70aaa41925
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2022-02-02 22:41:55 +0200

    Lab problem 1

--- AdvancedFunctions/Resources/01Aggregates/solve.js
+++ AdvancedFunctions/Resources/01Aggregates/solve.js
@@ -7,15 +7,20 @@ const input = [
 ];
 
 function aggregate(arr) {
-    console.log('Sum = ', reducer(arr, () => { }, 0));
-    // console.log('Min = ', reducer(arr, () => { }));
-    // console.log('Max = ', reducer(arr, () => { }));
-    // console.log('Product = ', reducer(arr, () => { }));
-    // console.log('Join = ', reducer(arr, () => { }));
+    console.log('Sum = ', reducer(arr, (a, b) => a + b, 0));
+    console.log('Min = ', reducer(arr, (a, b) => a < b ? a : b));
+    console.log('Max = ', reducer(arr, (a, b) => a > b ? a : b));
+    console.log('Product = ', reducer(arr, (a, b) => a * b, 1));
+    console.log('Join = ', reducer(arr, (a, b) => `${a}${b}`, ''));
 }
 
 function reducer(arr, operator, inital) {
+    const arrCopy = arr.slice(0);
     let result = inital;
+
+    if (result === undefined) {
+        result = arrCopy.shift();
+    }
 
     for (const element of arr) {
         result = operator(result, element);
