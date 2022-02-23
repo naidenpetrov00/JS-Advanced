af3c583f5f48435d92011ca50c8943d0f1856eab
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2022-02-23 21:38:27 +0200

    DOM manipulations lab

--- DOMManipulations/demo/app.js
+++ DOMManipulations/demo/app.js
@@ -1,7 +1,36 @@
-const texttemplate = document.createElement('p');
-texttemplate.style.background = 'black';
-texttemplate.style.color = 'white';
-texttemplate.className = 'special-text';
-texttemplate.innerHTML = 'TEXT';
+const createListItem = (function () {
+    const listItemTemplate = document.createElement('li');
+    listItemTemplate.className = 'list-item';
 
-document.body.appendChild(texttemplate);
+    const aTemplate = document.createElement('a');
+    aTemplate.href = '#';
+    listItemTemplate.appendChild(aTemplate);
+
+    return (text) => {
+        aTemplate.innerHTML = text;
+        const actualListItem = listItemTemplate.cloneNode(true);
+        document.querySelector('.list')
+            .appendChild(actualListItem);
+    };
+}());
+
+const addNewListItem = () => {
+    const textElement = document.querySelector('#tb-text');
+    const text = textElement.value;
+    textElement.value = '';
+    createListItem(text);
+}
+
+const deleteListItem = (ev) => {
+    const listItemToDelete = ev.target.parentNode;
+    document.querySelector('.list')
+        .removeChild(listItemToDelete);
+};
+
+window.onload = () => {
+    document.querySelector('#btn-add')
+        .addEventListener('click', addNewListItem);
+
+    document.querySelector('.list')
+        .addEventListener('click', deleteListItem);
+};
