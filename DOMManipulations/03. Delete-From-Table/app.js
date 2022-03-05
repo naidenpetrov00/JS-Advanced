1f8d236de48ad1205949d575b1dc1f3ee743b9c1
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2022-03-05 19:44:01 +0200

    DOM Manipulations lab problem 3

--- DOMManipulations/03. Delete-From-Table/app.js
+++ DOMManipulations/03. Delete-From-Table/app.js
@@ -1,3 +1,25 @@
-function deleteByEmail() {
-    console.log('TODO:...');
-}
+const SELECTORS = {
+    email: '[name=email]',
+    rows: '#customers tbody tr',
+};
+
+const getEmail = () => document.querySelector(SELECTORS.email).value;
+
+const getRows = () => document.querySelector(SELECTORS.rows)
+
+const getRowToDelete = (rows, email) => {
+    
+};
+
+const deleteRow = (row) => row.remove();
+
+const clearEmail =
+    () => document.querySelector(SELECTORS.email) = '';
+
+const deleteByEmail = () => {
+    const email = getEmail();
+    const rows = getRows();
+    const rowToDelete = getRowToDelete(rows, email);
+    deleteRow(rowToDelete);
+    clearEmail();
+};
