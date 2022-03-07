c78afe259212e4ce6096dd3e42ef177645c6a541
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2022-03-07 14:36:08 +0200

    dom lab problem 3

--- DOMManipulations/03. Delete-From-Table/app.js
+++ DOMManipulations/03. Delete-From-Table/app.js
@@ -2,11 +2,17 @@ const SELECTORS = {
     email: '[name=email]',
     rows: '#customers tbody tr',
     emailCell: '.email-cell',
+    notification: '#result',
+};
+
+const NOTIFICATION_TEXTS = {
+    success: 'Deleted.',
+    error: 'Not found.'
 };
 
 const getEmail = () => document.querySelector(SELECTORS.email).value;
 
-const getRows = () => document.querySelector(SELECTORS.rows)
+const getRows = () => document.querySelectorAll(SELECTORS.rows)
 
 const getRowToDelete = (rows, email) =>
     [...rows].find(row =>
@@ -15,12 +21,27 @@ const getRowToDelete = (rows, email) =>
 const deleteRow = (row) => row.remove();
 
 const clearEmail =
-    () => document.querySelector(SELECTORS.email) = '';
+    () => document.querySelector(SELECTORS.email).value = '';
+
+const setNotification = (text) =>
+    document.querySelector(SELECTORS.notification)
+        .innerHTML = text;
+
+const setSuccessfullNotification = () =>
+    setNotification(NOTIFICATION_TEXTS.success);
+
+const setErrorNotification = () =>
+    setNotification(NOTIFICATION_TEXTS.error);
 
 const deleteByEmail = () => {
     const email = getEmail();
     const rows = getRows();
     const rowToDelete = getRowToDelete(rows, email);
-    deleteRow(rowToDelete);
+    if (rowToDelete) {
+        deleteRow(rowToDelete);
+        setSuccessfullNotification();
+    } else {
+        setErrorNotification();
+    }
     clearEmail();
 };
