1f2f05e7616dc2611e95db7b6ba56daec654f2d8
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2022-03-07 19:38:01 +0200

    Lab problem 6

--- DOMManipulations/06. Highlight-Active/app.js
+++ DOMManipulations/06. Highlight-Active/app.js
@@ -1,3 +1,17 @@
 function focus() {
-    console.log('TODO:...');
+    const onFocus = function () {
+        this.parentNode.classList.add('focused');
+    };
+
+    const onBlur = function () {
+        this.parentNode.classList.remove('focused');
+
+    };
+
+    [...document.querySelectorAll('input')]
+        .forEach(input => {
+            input.addEventListener('focus', onFocus);
+            input.addEventListener('blur', onBlur);
+        });
+
 }
