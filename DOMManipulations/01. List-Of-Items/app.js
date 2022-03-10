0a1c7df4bb441c5da7634dca9fd1baf929f00c8e
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2022-03-10 19:38:16 +0200

    DOM Lab problem 1

--- DOMManipulations/01. List-Of-Items/app.js
+++ DOMManipulations/01. List-Of-Items/app.js
@@ -1,4 +1,17 @@
 // @ts-nocheck
 function addItem() {
-    console.log('TODO:...');
+    const SELECTORS = {
+        input: '#newItemText',
+        resultList: '#items',
+    }
+
+    const getInput = () => document.querySelector(SELECTORS.input).value;
+
+    const getResultList = () => document.querySelector(SELECTORS.resultList);
+
+    const input = document.createElement('li');
+    input.textContent = getInput();
+    const result = getResultList();
+    result.appendChild(input)
 }
+
