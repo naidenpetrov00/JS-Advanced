0023f49c9016ac4bc22d7f49673f15e13a076900
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2022-03-17 22:29:42 +0200

    probem 7

--- ExerciseDOMManipulations/07. Locked-Profile/app.js
+++ ExerciseDOMManipulations/07. Locked-Profile/app.js
@@ -1,3 +1,17 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const SELECTORS = {
+        buttons: 'button',
+        clickEvent: 'click',
+    };
+
+    const buttonElements = document.querySelectorAll(SELECTORS.buttons);
+
+    for (const button of buttonElements) {
+        button.addEventListener(SELECTORS.clickEvent, () => {
+
+            const hidenFilesId = button.parentElement.querySelector('div').id;
+            const hidenFilesElement = button.parentElement.querySelector(`#${hidenFilesId}`);
+            hidenFilesElement.style.display = 'block';
+        })
+    }
 }
