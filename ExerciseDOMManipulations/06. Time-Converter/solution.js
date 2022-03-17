774e1b28726c9b37b5d2e267b75e1b703ba31d4c
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2022-03-17 21:21:10 +0200

    problem 6

--- ExerciseDOMManipulations/06. Time-Converter/solution.js
+++ ExerciseDOMManipulations/06. Time-Converter/solution.js
@@ -1,4 +1,36 @@
 function attachEventsListeners() {
+    const SELECTORS = {
+        buttons: '[type=button]',
+        inputs: '[type=text]',
+    };
 
-    console.log('TODO:...');
+    const inputElements = document.querySelectorAll(SELECTORS.inputs)
+    const buttonElements = document.querySelectorAll(SELECTORS.buttons);
+
+    const inputs = {
+        daysInput: inputElements[0],
+        minutesInput: inputElements[1],
+        hoursInput: inputElements[2],
+        secondsInput: inputElements[3],
+    }
+
+    console.log(inputs);
+
+    for (const button of buttonElements) {
+        button.addEventListener('click', () => {
+            converter(button.id);
+        });
+    }
+
+    function converter(id) {
+        if (id === 'daysBtn') {
+            
+        } else if (id === 'hoursBtn') {
+
+        } else if (id === 'minutesBtn') {
+
+        } else if (id === 'secondsBtn') {
+
+        }
+    }
 }
