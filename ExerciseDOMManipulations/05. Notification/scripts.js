8979cc6c28d2b3a3a3033c7ef3fb9ae3b3658e25
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2022-03-17 19:12:13 +0200

    problem 5

--- ExerciseDOMManipulations/05. Notification/scripts.js
+++ ExerciseDOMManipulations/05. Notification/scripts.js
@@ -1,3 +1,16 @@
 function notify(message) {
-    console.log('TODO:...');
+    const SELECTORS = {
+        notification: '#notification',
+    };
+    const STYLE = {
+        hideDisplay: 'none',
+        showDisplay: 'block',
+        timeOut: 2000,
+    }
+
+    const notificationElement = document.querySelector(SELECTORS.notification);
+    notificationElement.textContent = message;
+    notificationElement.style.display = STYLE.showDisplay;
+
+    setTimeout(() => { notificationElement.style.display = STYLE.hideDisplay; }, STYLE.timeOut);
 }
