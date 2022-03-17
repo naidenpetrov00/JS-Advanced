91a4c71753e2058bb163a6554d65c0b2f4ad1e22
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2022-03-17 18:27:01 +0200

    problem 4

--- ExerciseDOMManipulations/04. Sections/script.js
+++ ExerciseDOMManipulations/04. Sections/script.js
@@ -1,3 +1,34 @@
 function create(words) {
-   console.log('TODO:...');
+   const SELECTORS = {
+      content: "#content",
+      div: 'div',
+      p: 'p',
+      clickEvent: 'click',
+      dblClickEvent: 'dblclick',
+   };
+   const STYLES = {
+      hideDisplay: 'none',
+      showDisplay: 'inline-block',
+      redColor: 'red',
+   }
+
+   let container = document.querySelector(SELECTORS.content);
+
+   for (const word of words) {
+      const divElement = document.createElement(SELECTORS.div);
+      const pElement = document.createElement(SELECTORS.p);
+
+      pElement.textContent = word;
+      pElement.style.display = STYLES.hideDisplay;
+
+      divElement.appendChild(pElement);
+      divElement.addEventListener(SELECTORS.clickEvent, () => {
+         pElement.style.display = STYLES.showDisplay;
+      });
+      divElement.addEventListener(SELECTORS.dblClickEvent, () => {
+         pElement.style.color = STYLES.redColor;
+      })
+
+      container.appendChild(divElement);
+   }
 }
