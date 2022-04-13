e1c8d4735c4e48992117f144636fe4165b7feec8
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2022-04-14 00:39:25 +0300

    Tests

--- JSClasses/StringBuilder/tests/tests.js
+++ JSClasses/StringBuilder/tests/tests.js
@@ -1,1 +1,24 @@
-const stringBuilder = require('../solution');
+const inputs = {
+    Hello: 'Hello',
+};
+
+const expect = require('chai').expect;
+const StringBuilder = require('../solution');
+
+describe('String Builder', function () {
+    let emtyStringBuilder = new StringBuilder();
+    let stringBuilder = new StringBuilder(inputs.Hello);
+
+    beforeEach(function () {
+    });
+
+    it('Can be instantiated with a passed in string argument or without anything', function () {
+        expect(stringBuilder).to.be.instanceOf(StringBuilder);
+        expect(emtyStringBuilder).to.be.instanceOf(StringBuilder);
+    })
+
+    it('append func should convert and add to the end of the storage', function () {
+        stringBuilder.append(inputs.Hello);
+        expect(stringBuilder._stringArray).to.have.length.above(0);
+    })
+});
